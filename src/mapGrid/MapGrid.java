ca6fff57bf7b74efae8379ec930f8989132d401f
DimitriosTrakos <dimitrios.trakoss@gmail.com>
2025-05-13 00:14:45 +0300

    fix astar

--- src/mapGrid/MapGrid.java
+++ src/mapGrid/MapGrid.java
@@ -100,4 +100,8 @@ public class MapGrid {
             System.out.println();
         }
     }
+
+    public boolean inBounds(int x, int y) {
+        return x >= 0 && x < width && y >= 0 && y < height;
+    }
 }
